8ec118f387ff6ac2a42821afaa3299b7ebc55d30
Yasen Trayanov <yasen.trayanov@industria.tech>
2019-12-20 13:12:22 +0200

    adding builded react app site files

---
+++ precache-manifest.0bafe04e54fbc6d7815cf811b67cacba.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7acebea993ea13ff81296c4d937150dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e8bdb8b5c1033d59e63",
+    "url": "/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "f013ae0324c2f9796666",
+    "url": "/static/css/main.a2938198.chunk.css"
+  },
+  {
+    "revision": "1e8bdb8b5c1033d59e63",
+    "url": "/static/js/2.d8fa79d5.chunk.js"
+  },
+  {
+    "revision": "f013ae0324c2f9796666",
+    "url": "/static/js/main.67db3ad7.chunk.js"
+  },
+  {
+    "revision": "8785e1944936c92fe174",
+    "url": "/static/js/runtime~main.956c73c7.js"
+  },
+  {
+    "revision": "2ba25893e2d0af37d72d43afb32a9635",
+    "url": "/static/media/jj-ying-7JX0-bfiuxQ-unsplash.2ba25893.jpg"
+  },
+  {
+    "revision": "3ac50b5b36eb2f11b000dce1792d0bb0",
+    "url": "/static/media/materialdesignicons-webfont.3ac50b5b.ttf"
+  },
+  {
+    "revision": "7ec5dab7e7ff250971d2ff50379778dc",
+    "url": "/static/media/materialdesignicons-webfont.7ec5dab7.woff2"
+  },
+  {
+    "revision": "a0d13d16cc2f3647680d9f1ff003f58b",
+    "url": "/static/media/materialdesignicons-webfont.a0d13d16.woff"
+  },
+  {
+    "revision": "a32fa1f27abbfa96ff2f79e1ade723d5",
+    "url": "/static/media/materialdesignicons-webfont.a32fa1f2.eot"
+  },
+  {
+    "revision": "239cf85916292b3585d17ba1f7801dbb",
+    "url": "/static/media/rubik-latin-300.239cf859.woff2"
+  },
+  {
+    "revision": "73ce1e5f9e44d490f48490673038d5d3",
+    "url": "/static/media/rubik-latin-300.73ce1e5f.woff"
+  },
+  {
+    "revision": "9578c8a599ff9b3b328f306da81607f0",
+    "url": "/static/media/rubik-latin-300italic.9578c8a5.woff2"
+  },
+  {
+    "revision": "b7a94712079cba234be3f546cf42f588",
+    "url": "/static/media/rubik-latin-300italic.b7a94712.woff"
+  },
+  {
+    "revision": "63c5b6176f60881d53dd2dc10904a04c",
+    "url": "/static/media/rubik-latin-400.63c5b617.woff2"
+  },
+  {
+    "revision": "80a9477d95ed5ca6e64843c1024e9e7f",
+    "url": "/static/media/rubik-latin-400.80a9477d.woff"
+  },
+  {
+    "revision": "4ff19ede4cf2a2a07c638795a389f6df",
+    "url": "/static/media/rubik-latin-400italic.4ff19ede.woff2"
+  },
+  {
+    "revision": "f778b3151e4b3ed5b831e9e8646ab207",
+    "url": "/static/media/rubik-latin-400italic.f778b315.woff"
+  },
+  {
+    "revision": "9ac3d9de00cce6d3352a889816226451",
+    "url": "/static/media/rubik-latin-500.9ac3d9de.woff2"
+  },
+  {
+    "revision": "bec9243368f86e688b23bc1852823d0b",
+    "url": "/static/media/rubik-latin-500.bec92433.woff"
+  },
+  {
+    "revision": "21955254e41869cea3f5a1769ab1cbe6",
+    "url": "/static/media/rubik-latin-500italic.21955254.woff"
+  },
+  {
+    "revision": "d8de1d9a8a017aed7e74649e8e06eeb5",
+    "url": "/static/media/rubik-latin-500italic.d8de1d9a.woff2"
+  },
+  {
+    "revision": "29b164482600fc3976b5b2c863d69b71",
+    "url": "/static/media/rubik-latin-700.29b16448.woff"
+  },
+  {
+    "revision": "a7db29488272756e766b024e6aebe848",
+    "url": "/static/media/rubik-latin-700.a7db2948.woff2"
+  },
+  {
+    "revision": "4a8c46be9056c47cc8d0e3a3a70ae193",
+    "url": "/static/media/rubik-latin-700italic.4a8c46be.woff"
+  },
+  {
+    "revision": "9c1d6e0e66641c7c0806a5ad1b3d9286",
+    "url": "/static/media/rubik-latin-700italic.9c1d6e0e.woff2"
+  },
+  {
+    "revision": "0d11544364bc4541af96267ca8b2a5ce",
+    "url": "/static/media/rubik-latin-900.0d115443.woff2"
+  },
+  {
+    "revision": "44913051ee3d4536cd4fa368067e9a83",
+    "url": "/static/media/rubik-latin-900.44913051.woff"
+  },
+  {
+    "revision": "3c9d154e1fa888849c63a616e2f09b3c",
+    "url": "/static/media/rubik-latin-900italic.3c9d154e.woff2"
+  },
+  {
+    "revision": "5b173e43e6e846589858d97fa0b406c9",
+    "url": "/static/media/rubik-latin-900italic.5b173e43.woff"
+  }
+]);
